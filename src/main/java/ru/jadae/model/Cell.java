3027fcc7cc9a25c17a5f43d0a0767eadf71e5fa6
jadae <jadaevmatvei@gmail.com>
2024-04-12 18:20:48 +0300

    Added neighbour check for cell

--- src/main/java/ru/jadae/model/Cell.java
+++ src/main/java/ru/jadae/model/Cell.java
@@ -12,10 +12,13 @@ public class Cell {
 
     private boolean isActive = false;
     private List<Cell> neighbours = new ArrayList<>();
-
+    private final int heightPos;
+    private final int widthPos;
     private Character cellValue;
 
-    public Cell() {
+    public Cell(int heightPos, int widthPos) {
+        this.heightPos = heightPos;
+        this.widthPos = widthPos;
     }
 
     public void addNeighbour(Cell cell) {
@@ -24,9 +27,13 @@ public class Cell {
 
     public boolean hasFilledNeighbours() {
         for (Cell cell : neighbours) {
-            if (cell.getCellValue() == null) return false;
+            if (cell.getCellValue() != null) return true;
         }
-        return true;
+        return false;
+    }
+
+    public boolean isNeighbourToCell(Cell cell) {
+        return this.neighbours.contains(cell);
     }
 
 }
