b59b2e7fb59129744d91b6fa6a3caaf56ac9fbef
IconMatthew <jadaevmatvei@gmail.com>
2024-05-14 21:21:01 +0300

    Changed the way application starts

--- src/main/java/ru/jadae/Main.java
+++ src/main/java/ru/jadae/Main.java
@@ -1,15 +1,11 @@
 package ru.jadae;
 
-import ru.jadae.builder.GameBuilder;
 import ru.jadae.view.GameFrame;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-//        GameBuilder gameBuilder = new GameBuilder();
-//        gameBuilder.getGame().gameCycle();
         SwingUtilities.invokeLater(GameFrame::new);
-
     }
 }
