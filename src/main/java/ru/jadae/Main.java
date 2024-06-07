83e79ee03cededb6197315dc7a01ff8164f9a9aa
IconMatthew <jadaevmatvei@gmail.com>
2024-06-07 12:34:54 +0300

    VERSION 1.0

--- src/main/java/ru/jadae/Main.java
+++ src/main/java/ru/jadae/Main.java
@@ -4,6 +4,7 @@ import ru.jadae.view.GameFrame;
 
 import javax.swing.*;
 
+//Version 1.0
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GameFrame::new);
